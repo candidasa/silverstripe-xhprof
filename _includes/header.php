859d492a232bad9b028008f361716dbcf7e60172
Julian Seidenberg <julian@silverstripe.com>
2015-09-18 16:10:41 +1200

    going back to PHP 5.3 version of include

--- _includes/header.php
+++ _includes/header.php
@@ -3,5 +3,5 @@
 if (isset($_GET["xhprof"]) && extension_loaded('xhprof')) {
     include_once '../xhprof/xhprof_lib/utils/xhprof_lib.php';
     include_once '../xhprof/xhprof_lib/utils/xhprof_runs.php';
-    xhprof_enable(XHPROF_FLAGS_NO_BUILTINS);
+    xhprof_enable(XHPROF_FLAGS_CPU + XHPROF_FLAGS_MEMORY);
 }
