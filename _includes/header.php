cfdc4606efcaacb2bd4cd65a7c559039b1a98bce
Julian Seidenberg <julian@silverstripe.com>
2015-09-18 17:02:13 +1200

    BUG updating paths in header.php

--- _includes/header.php
+++ _includes/header.php
@@ -1,7 +1,7 @@
 <?php
 
 if (isset($_GET["xhprof"]) && extension_loaded('xhprof')) {
-    include_once '../xhprof/xhprof_lib/utils/xhprof_lib.php';
-    include_once '../xhprof/xhprof_lib/utils/xhprof_runs.php';
-    xhprof_enable(XHPROF_FLAGS_CPU + XHPROF_FLAGS_MEMORY);
+    include_once '../../xhprof/xhprof_lib/utils/xhprof_lib.php';
+    include_once '../../xhprof/xhprof_lib/utils/xhprof_runs.php';
+    xhprof_enable(XHPROF_FLAGS_NO_BUILTINS);
 }
